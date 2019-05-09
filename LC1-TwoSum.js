5efa3e5ac62d01931368bd74bdcc0773eb52484a
sherryhuiyuanai <sherryhuiyuanai@gmail.com>
2019-05-09 17:19:01 -0400

    version 1, basic succeed, fail on not using the same index twice

--- LC1-TwoSum.js
+++ LC1-TwoSum.js
@@ -1,6 +1,5 @@
 /*
-LeetCode 
-1. Two Sum
+LeetCode 1. Two Sum
 
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -12,30 +11,28 @@ Because nums[0] + nums[1] = 2 + 7 = 9, return [0, 1].
 
 
  // for testing
-let nums = [2, 7, 11, 15];
-let target = 9;
+let nums = [11, 15, 7, 2, 2];
+let target = 4;
 
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
-
 let twoSum = function (nums, target) {
     let index1;
     let index2;
     for(let i = 0; i < nums.length; i++){
-        let temp = target - nums[i];
+        let temp = target - nums[i]; //7
         for(let j = 0; j < nums.length; j++){
-            index2 = temp == nums[j]? j: null;
-            if(index2 == j){
+            if(temp == nums[j]){
+                index2 = j;
                 index1 = i;
             }
         }
     }
-    let indices = [index1, index2];
-
-    return indices;
+    return [index1, index2];
 };
 
+// see output
 console.log(twoSum(nums, target));
