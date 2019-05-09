f9641253193e3bba432dadb8514c69a4ba689026
sherryhuiyuanai <sherryhuiyuanai@gmail.com>
2019-05-09 17:34:36 -0400

    version 1+, almost fix the opposite order problem(previously, when index1 have value, the loop would not stop, it continued to iterate and wasted time, so finally the index1 always bigger than index 2, which is not optimized.)

--- LC1-TwoSum.js
+++ LC1-TwoSum.js
@@ -11,8 +11,8 @@ Because nums[0] + nums[1] = 2 + 7 = 9, return [0, 1].
 
 
  // for testing
-let nums = [11, 15, 7, 2, 2];
-let target = 4;
+let nums = [11, 15, 7, 2];
+let target = 9;
 
 /**
  * @param {number[]} nums
@@ -20,17 +20,29 @@ let target = 4;
  * @return {number[]}
  */
 let twoSum = function (nums, target) {
-    let index1;
-    let index2;
-    for(let i = 0; i < nums.length; i++){
-        let temp = target - nums[i]; //7
-        for(let j = 0; j < nums.length; j++){
-            if(temp == nums[j]){
+    let index1 = null;
+    let index2 = null;
+    // if(index1 == null){
+    //     for (let i = 0; i < nums.length; i++) {
+
+    //     }
+    // }
+    let i = 0;
+    while(index2 == null){
+
+        let gap = target - nums[i];
+        for (let j = 0; j < nums.length; j++) {
+            if (gap == nums[j]) {
                 index2 = j;
-                index1 = i;
+                console.log(index2, 222);
+                if (j !== i) {
+                    index1 = i;
+                }
             }
         }
+        i++;
     }
+
     return [index1, index2];
 };
 
