a78812be72e0a095fed2c113fff20df9c3be4349
sherryhuiyuanai <sherryhuiyuanai@gmail.com>
2019-05-10 23:39:00 -0400

    Basically finished structure but still bugs

--- LC3-longest-substring.js
+++ LC3-longest-substring.js
@@ -24,9 +24,32 @@ Explanation: The answer is "wke", with the length of 3.
  * @param {string} s
  * @return {number}
  */
-let lengthOfLongestSubstring = function (str) {
-    let array = Array(str.length);
-    for(let i = 0; i < str.length; i++){
+let lengthOfLongestSubstring = function (string) {
+    let array = Array(string.length);
+    let str = string.split("");
+    array[0] = str[0];
+
+    let len = 1;
+    let len_temp = 1;
+    let fail_index = 0;
+    for(let i = 1; i < str.length; i++){
+
+        array[i] = str[i] == str[i-1]? array[i]: str[i];
+
+        if (str[i] != str[i - 1] && !array.includes(str[i])){
+            array[i - fail_index - 1] = str[i];
+            len_temp = i - fail_index + 1;
+
+            if (len_temp > len){
+                len = len_temp;
+            }
+        } else {
+            fail_index = i;
+        }
         
     }
-};
+    return array;
+}
+
+str = "pwwkew";
+console.log(lengthOfLongestSubstring(str));
